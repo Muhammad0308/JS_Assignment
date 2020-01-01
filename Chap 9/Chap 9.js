8cf126b16298fe2a4b34a8ba77ccdc0edc86e256
Muhammad0308 <52210350+Muhammad0308@users.noreply.github.com>
2020-01-02 01:00:02 +0500

    Upload JS Assignment Chap 9

---
+++ Chap 9/Chap 9.js
@@ -0,0 +1,4 @@
+var firstName = prompt("Enter Your First Name","Muhammad Shariq");
+var lastName = prompt("Enter Your Last Name", "Khan");
+var age = prompt("Enter Your age","26");
+alert("Hello! "+firstName+" "+lastName+", You are "+age+" years old.");
