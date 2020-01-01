55d9681b45e69a1af9d8e9d3cf7beb952b60646a
Muhammad0308 <52210350+Muhammad0308@users.noreply.github.com>
2020-01-02 01:41:28 +0500

    Upload JS Assignment Chap 18 to 20

---
+++ Chap 18 to 20/Chap 18 to 20.js
@@ -0,0 +1,12 @@
+var courses=[]
+for(i=0 ; i <=4; i++){
+    courses[i] = prompt("Enter your subject.")
+    }
+    alert(courses);
+    for(i=0 ; i<= 4 ; i++){
+        alert(courses[i])
+    }
+    for(i=0 ; i<= 4 ; i++){
+    courses[i] = prompt("Please Enter Different Subject.");
+}
+alert(courses); 
