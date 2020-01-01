020e6077300080b0201acb0e6e2434bada109e2b
Muhammad0308 <52210350+Muhammad0308@users.noreply.github.com>
2020-01-02 01:36:32 +0500

    Upload JS Assignment Chap 15 to 17

---
+++ Chap 15 to 17/Chap 15 to 17.js
@@ -0,0 +1,13 @@
+var courses = [];
+courses[0] = prompt("Enter first course"); // English
+courses[1] = prompt("Enter second course"); // Urdu
+courses[2] = prompt("Enter third course"); // Python
+courses[3] = prompt("Enter fourth course"); // JavaScript
+courses[4] = prompt("Enter fifth course"); // Kubernetes
+alert(courses);
+courses[0] = prompt("Enter first Subject","English");
+courses[1] = prompt("Enter second Subject","Urdu");
+courses[2] = prompt("Enter third Subject","Python");
+courses[3] = prompt("Enter fourth Subject","JavaScript");
+courses[4] = prompt("Enter fifth Subject","Kubernetes");
+alert(courses);
